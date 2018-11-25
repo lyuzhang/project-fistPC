586af807c15ed2e39b73ada0d5b49b98e744e723
zhangjiewei <1525456439@qq.com>
2018-11-25 15:31:55 +0800

    自动轮播和背景图

--- src/js/firstView.js
+++ src/js/firstView.js
@@ -4,10 +4,12 @@
 export default function () {
   const pointsNodes = document.querySelectorAll('.home .home-points li');
   const carouselNodes = document.querySelectorAll('.home .home-carousel li');
+  const homeNode = document.querySelector('.home');
   const pointsLength = pointsNodes.length;
   let nowIndex = 0;
   let lastIndex = 0;
   let lastTime = 0;
+  let timer = null;
 
 
   for(let i = 0;i < pointsLength;i++){
@@ -36,4 +38,25 @@ export default function () {
       lastTime = nowTime;
     }
   }
+
+
+  //自动轮播
+  function autoPlay() {
+    timer = setInterval(() => {
+      nowIndex++;
+      if(nowIndex === 4) nowIndex = 0;
+      carouselNodes[lastIndex].className = 'common-title leftHide';
+      carouselNodes[nowIndex].className = 'common-title rightShow';
+      pointsNodes[nowIndex].className = 'active';
+      pointsNodes[lastIndex].className = '';
+      lastIndex = nowIndex;
+
+    },3000)
+  }
+autoPlay();
+  //绑定鼠标移入移除停止轮播事件
+  homeNode.onmouseenter = function () {
+    clearInterval(timer);
+  }
+  homeNode.onmouseleave = autoPlay;
 }
